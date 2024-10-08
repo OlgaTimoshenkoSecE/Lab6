d6d9296460ccc7ce1f88ef8adf0029b1cddcd604
OlgaTimoshenkoSecE <n01679143@humber.ca>
2024-10-08 08:55:58 -0400

    task 2
    completed task 2

--- lab6.js
+++ lab6.js
@@ -1,3 +1,14 @@
 let myNumber = 10;
 let myString = "Hello, JavaScript!";
 console.log(myNumber, myString);
+
+console.log("\n")
+let numb = []
+for (let i=0; i<11;i++){
+    console.log(i);
+}
+
+for (let n=1; n<6;n++){
+    console.log(`The ${n} multiple of 5 is: ${n*5}`);
+}
+console.log("\n")
