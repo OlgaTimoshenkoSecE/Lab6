c059451e3b5fc48f7f01ec49f0d2d0b816df9272
OlgaTimoshenkoSecE <n01679143@humber.ca>
2024-10-08 09:04:19 -0400

    task 3
    completed task 3 & added notes for code readabilty

--- lab6.js
+++ lab6.js
@@ -1,14 +1,23 @@
+//task 1
 let myNumber = 10;
 let myString = "Hello, JavaScript!";
 console.log(myNumber, myString);
-
 console.log("\n")
+//task 2
 let numb = []
 for (let i=0; i<11;i++){
     console.log(i);
 }
-
 for (let n=1; n<6;n++){
     console.log(`The ${n} multiple of 5 is: ${n*5}`);
 }
 console.log("\n")
+//task 3
+function addNumbers(num1, num2){
+    return num1+num2;
+}
+console.log(addNumbers(999,6897));
+function multiplyNumbers(num1,num2){
+    return num1*num2;
+}
+console.log(multiplyNumbers(88,67));
